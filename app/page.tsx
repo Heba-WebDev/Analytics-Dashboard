65e0dc57dff71151a8557c4bbb6fee7d95234ac6
Heba-Webdev23 <jtvonline@yahoo.com>
2024-01-26 18:56:18 +0300

    Added graphe and widgets components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import Graphe from "./components/home/Graphes";
+import Widgets from "./components/home/Widgets";
+
 export default function Home() {
   return (
     <main className=" container mx-auto px-2 py-6">
-      <h1>Content</h1>
+      <section className="flex flex-col-reverse lg:grid lg:grid-cols-5 items-center justify-between gap-5">
+        <Graphe />
+        <Widgets />
+      </section>
     </main>
   );
 }
